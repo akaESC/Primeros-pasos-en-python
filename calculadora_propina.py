a6a0cc53ba52527ff87964d3e19890b1e7b1f2d1
Fran <francistrueoficial@gmail.com>
2026-02-08 04:05:01 +0100

    Eliminando archivo de la nube

--- calculadora_propina.py
+++
@@ -1,5 +0,0 @@
-cuenta = float(input("¿Cuánto te ha costado toda la cuenta?: "))
-propina = float(input("¿Qué porcentaje de propina quieres dejar 10% o 15%?: "))
-propina_calculada = cuenta * (propina / 100)
-total = cuenta + propina_calculada
-print(f"Tu propina es de {propina_calculada}€ y el total a pagar es de {total}€.")
