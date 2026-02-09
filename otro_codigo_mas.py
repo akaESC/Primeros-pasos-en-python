7e711c87f086d4763b98bdb2d73feadf94e0271e
Fran <francistrueoficial@gmail.com>
2026-02-09 16:15:08 +0100

    Mochila para guardar basica

---
+++ otro_codigo_mas.py
@@ -0,0 +1,10 @@
+# Esto es una lista vacía
+nombres = []
+
+# Así se guardan cosas sin borrar lo anterior
+nombres.append("Fran")
+nombres.append("Juan")
+nombres.append("Sergio")
+nombres.append("Carlos")
+
+print(nombres) # Resultado: ["Fran", "Juan"]
---
+++ practica_listas.py
@@ -0,0 +1,10 @@
+# 1. Creamos una lista vacía
+mochila = []
+# 2. Pedimos dos cosas para guardar en la mochila
+objeto1 = input("¿Qué quieres guardar primero en la mochila? ")
+mochila.append(objeto1)
+objeto2 = input("¿Qué quieres guardar después en la mochila? ")
+mochila.append(objeto2)
+# 3. Vemos qué hay dentro de la mochila que hemos creado
+print("En tu mochila tienes guardado:")
+print(mochila)
